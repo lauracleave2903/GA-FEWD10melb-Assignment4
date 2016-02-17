aa8a897662f06e9e824c5c99e5cfe28b48bff6d4
Laura <lauracleave29@gmail.com>
2016-02-17 21:03:42 +1100

    Second-Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
 
+    //var city = ? ;
+
+    $('#submit-btn').click(function () {
+        $('#city-type').val();
+        if (city == "New York" || city == "New York City" || city == "NY");
+        $('body').addClass('nyc');
+    });
+    $('#submit-btn').click(function () {
+        $('#city-type').val();
+        if (city == "San Francisco" || city == "SF" || city == "Bay Area");
+        $('body').addClass('sf');
+    });
+
 });
